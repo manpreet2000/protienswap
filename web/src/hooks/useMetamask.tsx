cd71a5ea22064722e72cd86e1d2df512c2344abd
Manpreet Singh <manpreet.singh@block8.com>
2021-12-23 23:14:08 +0530

    metamask hook

--- web/src/hooks/useMetamask.tsx
+++ web/src/hooks/useMetamask.tsx
@@ -1,7 +1,7 @@
 import {  useWeb3React } from "@web3-react/core";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { InjectedConnector } from "@web3-react/injected-connector";
-
+import { EtherService } from "../services/etherService";
 
 const chainID = process.env.CHAIN_ID;
 if(!chainID) throw new Error("Chain ID not present in env");
@@ -9,5 +9,31 @@ const injector = new InjectedConnector({supportedChainIds: [+chainID]});
 export const useMetamask = () =>{
     const web3utils = useWeb3React();
     const [loading,setLoading] = useState(false);
+    const instance = EtherService.getInstance();
 
+    useEffect(()=>{
+        injector.isAuthorized().then((t)=>{
+            if(t){
+                web3utils.activate(injector,()=>{throw new Error("Change Network")});
+            }
+        });
+        if(web3utils.account){
+            instance.setCurrentAccount(web3utils.account);
+        }
+    },[web3utils,instance]);
+
+    const metamaskConnect = async ()=>{
+        setLoading(true);
+        try{
+            await web3utils.activate(injector);
+            setLoading(false);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+    return {
+        loading,
+        metamaskConnect
+    }
 }
