4d9f2896af9dbbd76e4d163968daecbd6496a947
Manpreet Singh <manpreet.singh@block8.com>
2022-01-08 16:37:57 +0530

    protien services

--- web/src/services/etherService.ts
+++ web/src/services/etherService.ts
@@ -80,7 +80,8 @@ export class EtherService {
             this.provider.getSigner(0) as ethers.providers.JsonRpcSigner,
         );
         const balance:ethers.BigNumber = await erc20Instance.allowance(ownerAddress,spenderAddress);
-        return ethers.utils.formatUnits(balance,token.decimal);
+        const decimals = await erc20Instance.decimals();
+        return ethers.utils.formatUnits(balance,decimals);
     }
     public async balanceOf(token: Token, address: string): Promise<string> {
         const tokenInstance = ERC20__factory.connect(
@@ -111,7 +112,22 @@ export class EtherService {
         return tx;
       }
     
-
+    async getTokenfromAddr(addr:string):Promise<Token>{
+        const erc20Instance = ERC20__factory.connect(
+            addr,
+            this.provider?.getSigner(0) as ethers.providers.JsonRpcSigner,
+          );
+        const name=await erc20Instance.name();
+        const sym = await erc20Instance.symbol();
+        const dec = await erc20Instance.decimals();
+        return {
+            name:name,
+            symbol:sym,
+            logoUrl:"",
+            address:addr,
+            decimal:dec
+        }
+    }
 
 
 }
