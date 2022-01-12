a89f5fc912f5199c356c4c62b06f11dc5987d92d
Lakshay Khurana <lakshay.khurana@block8.com>
2022-01-12 14:24:33 +0530

    branch push

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,16 @@
-import { MuiThemeProvider } from "./theme";
 import { Web3ReactProvider } from "@web3-react/core";
 import { EtherService } from "./services/etherService";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
-export const App: React.FC = () => 
-    <MuiThemeProvider>
-      <Web3ReactProvider getLibrary={() => EtherService.getInstance().getProvider()}>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </BrowserRouter>
-      </Web3ReactProvider>
-    </MuiThemeProvider>;
+export const App: React.FC = () => (
+  <Web3ReactProvider
+    getLibrary={() => EtherService.getInstance().getProvider()}
+  >
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </Web3ReactProvider>
+);
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Button } from "@mui/material";
 import { useMetamask } from "../hooks/useMetamask";
 import { EtherService } from "../services/etherService";
-const Home: React.FC= () =>{
-    const {active,loading,metamaskConnect} = useMetamask();
-    const account = EtherService.getInstance().userAccount;
-    console.log({active,loading,account});
-    return(
-        <Button onClick={metamaskConnect}>
-        click me 
-        </Button>
-    )
+import { Box } from "../components/Box";
+const Home: React.FC = () => {
+  const { active, loading, metamaskConnect } = useMetamask();
+  const account = EtherService.getInstance().userAccount;
+  console.log({ active, loading, account });
+  return (
+    <>
+      <Button onClick={metamaskConnect}>click me</Button>
+      <Box />
+    </>
+  );
 };
 
 export default Home;
