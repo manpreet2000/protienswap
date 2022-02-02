f1a7a031cf10fc208017ecc489da5576f7eae3de
Manpreet Singh <manpreet.singh@block8.com>
2022-02-03 01:07:02 +0530

    added contextApi

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import { Web3ReactProvider } from "@web3-react/core";
 import { EtherService } from "./services/etherService";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MuiThemeProvider } from "./theme";
+import SwapContextProvider from "./context/SwapContext";
 import Home from "./pages/Home";
 
 export const App: React.FC = () => (
@@ -9,11 +10,13 @@ export const App: React.FC = () => (
     <Web3ReactProvider
       getLibrary={() => EtherService.getInstance().getProvider()}
     >
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <SwapContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </SwapContextProvider>
     </Web3ReactProvider>
   </MuiThemeProvider>
 );
--- web/src/components/liquidityHeader/LiquidityHeader.tsx
+++ web/src/components/liquidityHeader/LiquidityHeader.tsx
@@ -1,32 +1,53 @@
 import { Box } from "@mui/material";
+import { useContext } from "react";
+import { CURRENTMODAL, SwapContext } from "../../context/SwapContext";
 
 const LiquidityHeader = () => {
-    const styles = {
-        "color":"background.paper",
-        "padding":"0 10px 0 10px",
-        "marginLeft":"5px",
-        "marginRight":"5px",
-        "alignSelf":"center",
-        "backgroundColor":"secondary.main",
-        "zoom":"1.3",
-        "borderRadius":"10px",
-        "cursor":"pointer",
-        "&:hover":{
-            "backgroundColor":"primary.main"
-        }
-    }
-  return(
-      <Box
+  const styles = {
+    color: "background.paper",
+    padding: "0 10px 0 10px",
+    marginLeft: "5px",
+    marginRight: "5px",
+    alignSelf: "center",
+    backgroundColor: "secondary.main",
+    zoom: "1.3",
+    borderRadius: "10px",
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "primary.main",
+    },
+  };
+
+  const { currentModal, changeCurrentModal } = useContext(SwapContext);
+  return (
+    <Box
       sx={{
-          display:"flex",
-          backgroundColor:"primary.main",
-          padding:"0 20px 0 20px",
-          marginTop:"1rem",
-        borderRadius:"10px"
-      }}>
-          <Box sx={styles}>Swap</Box>
-          <Box sx={styles}>Pool</Box>
+        display: "flex",
+        backgroundColor: "primary.main",
+        padding: "0 20px 0 20px",
+        marginTop: "1rem",
+        borderRadius: "10px",
+      }}
+    >
+      <Box
+        sx={{
+          ...styles,
+          transform: currentModal === CURRENTMODAL.SWAP ? "rotate(14deg)" : "",
+        }}
+        onClick={() => changeCurrentModal(CURRENTMODAL.SWAP)}
+      >
+        Swap
       </Box>
+      <Box
+        sx={{
+          ...styles,
+          transform: currentModal === CURRENTMODAL.POOL ? "rotate(14deg)" : "",
+        }}
+        onClick={() => changeCurrentModal(CURRENTMODAL.POOL)}
+      >
+        Pool
+      </Box>
+    </Box>
   );
 };
 
